b77d2d33fd2f683af9f1dc65d4a84c18a79dd133
sabinaamanova <98976670+sabinaamanova@users.noreply.github.com>
2023-03-25 13:24:25 +0600

    Add files via upload

---
+++ lab7/informatics/input-output/b.py
@@ -0,0 +1,6 @@
+import math
+a = int(input())
+b = a+1
+c = a-1
+print("The next number for the number", a, "is" ,b, end="." )
+print("\nThe previous number for the number", a, "is", c,end=".")
---
+++ lab7/informatics/input-output/e.py
@@ -0,0 +1,4 @@
+v = int(input())
+t = int(input())
+c = ((v*t)%109) 
+print(c)
